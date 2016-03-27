99e7a7d51ac9bc0b658cc6d1815bc73f6175d304
Ivan Galeano <ilgaleanos@yahoo.com>
2016-03-27 17:27:22 -0500

    cambios simples

--- example_ws.py
+++ example_ws.py
@@ -13,11 +13,11 @@ test =  [x for x in itertools.permutations('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890
 random.shuffle(test)
 for source, target in test:
 
-    node_source = graph.Node(source)
-    node_target = graph.Node(target)
+	node_source = graph.Node(source)
+	node_target = graph.Node(target)
 
-    stream.add_node(node_source,node_target)
-    # time.sleep(0.5) # Make it slower :D
-    stream.add_edge(graph.Edge(node_source,node_target))
+	stream.add_node(node_source,node_target)
+	# time.sleep(0.5) # Make it slower :D
+	stream.add_edge(graph.Edge(node_source,node_target))
 
 time.sleep(1) #It might be possible the script runs too fast and last action anr't sent properly
